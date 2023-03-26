262f0f1ecd6a097b5968282dfa621ff2432ad25a
postema <pj.postema@gmail.com>
2023-03-26 15:40:35 +0200

    add subject, limit newsitems, filter on language

--- main.py
+++ main.py
@@ -5,26 +5,21 @@ from send_email import send_email
 
 api_key = os.getenv("API_KEY_NEWS")
 
-url = f"https://newsapi.org/v2/everything?q=tesla&from=2023-03-25&sortBy=publishedAt&apiKey={api_key}"
+subject = "daily news from API"
+topic = "gold"
+
+url = f"https://newsapi.org/v2/everything?q={topic}&language=en&from=2023-03-25&sortBy=publishedAt&apiKey={api_key}"
 
 request = requests.get(url)
-content = request.json()
+response = request.json()
 
-raw_message = ""
-for article in content["articles"]:
+raw_message = f"Subject: {subject}" + "\n"
+for article in response["articles"][:10]:
     if article["title"] is not None:
         raw_message = raw_message + (article["title"]) + "\n"
     raw_message = raw_message + (article["description"]) + "\n"
     raw_message = raw_message + (article["url"]) + "\n\n"
-# raw_message = raw_message.encode('utf-8')
+raw_message = raw_message.encode('utf-8')
 
-message = f"""\
-Subject: daily news
-
-From: news API
-
-{raw_message}
-"""
-
-send_email(message)
-print(message)
+send_email(raw_message)
+print(raw_message)
--- send_email.py
+++ send_email.py
@@ -9,14 +9,15 @@ def send_email(message):
 
     username = "pj.postema@gmail.com"
     password = os.getenv("PASSWORD")
+
     receiver = "pj.postema@gmail.com"
-    subject = "daily news from API"
     context = ssl.create_default_context()
 
-    fmt = 'From: {}\r\nTo: {}\r\nSubject: {}\r\n{}'
+    fmt = 'From: {}\r\nTo: {}\r\n{}'
+    # fmt = 'From: {}\r\nTo: {}\r\nSubject: {}\r\n{}'
 
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
-        # server.sendmail(username, receiver, message)
-        server.sendmail(username, receiver, fmt.format(username, receiver, subject, message).encode('utf-8'))
+        server.sendmail(username, receiver, message)
+        # server.sendmail(username, receiver, fmt.format(username, receiver, message).encode('utf-8'))
